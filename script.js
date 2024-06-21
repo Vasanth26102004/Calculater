63a87a906cd4869ddf6b0bf0cd79066eb52e0caf
Vasanth26102004 <vasanth.p2610@gmil.com>
2024-06-21 14:06:44 +0530

    final changes

--- script.js
+++ script.js
@@ -1,52 +1,31 @@
-let btn9 = document.getElementById('9');
-let btn8 = document.getElementById('8');
-let btn7 = document.getElementById('7');
-let btn6 = document.getElementById('6');
-let btn5 = document.getElementById('5');
-let btn4 = document.getElementById('4');
-let btn3 = document.getElementById('3');
-let btn2 = document.getElementById('2');
-let btn1 = document.getElementById('1');
-let btn0 = document.getElementById('0');
-let btn = document.getElementById('.');
+let input = document.querySelector(".input");
+let output = document.querySelector(".output");
 
-let btnAdd = document.getElementById('+');
-let btnSub = document.getElementById('-');
-let btnMul = document.getElementById('x');
-let btnDiv = document.getElementById('/');
-let btnEql = document.getElementById('=');
+let Equa = "";
 
-let btnClear = document.getElementById('C');
+input.textContent = "";
+output.textContent = "0";
 
-let input = document.querySelector('.input');
-let output = document.querySelector('.output');
-
-output.textContent = "0"
-
-function AppendValue(value){
-    input.value += value;
-
+function AppendValue(value) {
+  input.textContent = Equa + value;
+  Equa = input.textContent
+  output.textContent = eval(Equa);
+  output.style.color = '#949494';
 }
 
-function ResultValue(){
-output.textContent=eval(input.value)
+function ResultValue() {
+  output.textContent = eval(Equa);
+  Equa = output.textContent
+  output.style.color = '#000';
 }
 
-function ClearValue(){
-
-    input.value = "";
-    output.textContent = "0"
+function ClearValue() {
+  input.textContent = "";
+  Equa = ""
+  output.textContent = "0";
 }
 
-function DeleteValue(){
-
+function DeleteValue() {
+    Equa = Equa.substring(0, Equa.length-1);
+    input.textContent = input.textContent.substring(0, input.textContent.length-1);
 }
-
-
-
-
-
-
-
-
-
